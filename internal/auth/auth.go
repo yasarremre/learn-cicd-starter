cc4f484aad25da7f11a2428f307d6dbd7e62e7db
yasarremre <yasarr.07@outlook.com>
2026-04-03 10:20:48 +0300

    fix auth.go

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -8,18 +8,15 @@ import (
 
 var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 
-// GetAPIKey - BROKEN VERSION FOR CI TESTING
+// GetAPIKey extracts an API Key from
+// the HTTP request headers
 func GetAPIKey(headers http.Header) (string, error) {
 	authHeader := headers.Get("Authorization")
 	if authHeader == "" {
 		return "", ErrNoAuthHeaderIncluded
 	}
 	splitAuth := strings.Split(authHeader, " ")
-	
-	// We broke this line: changed "ApiKey" to "Bearer"
-	// This will cause the unit tests to return a "malformed authorization header" error
-	// instead of the expected API Key.
-	if len(splitAuth) < 2 || splitAuth[0] != "Bearer" {
+	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
 		return "", errors.New("malformed authorization header")
 	}
 
