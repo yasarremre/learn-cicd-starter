94c22fd876c9cdda6d3d36a4aec7bdf43c6d36f6
yasarremre <yasarr.07@outlook.com>
2026-04-03 12:17:51 +0300

    fix main.go

--- main.go
+++ main.go
@@ -95,6 +95,6 @@ func main() {
 		ReadHeaderTimeout: 10 * time.Second,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving on port: %s\n", port) // #nosec G706
 	log.Fatal(srv.ListenAndServe())
 }
